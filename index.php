68474e4af60dffaad959a21b839f1832cc07dfe3
Marc Menéndez Simó <159684158+marc-ms@users.noreply.github.com>
2025-03-26 22:34:46 +0100

    php blog project added

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="logo__container">
+            <a href="index.php">
+                VIDEO GAMES BLOG
+            </a>
+        </div>
+
+        <!-- MENU -->
+        <nav class="nav__container">
+            <a href="">INICIO</a>
+            <a href="">CATEGORY 1</a>
+            <a href="">CATEGORY 2</a>
+            <a href="">CATEGORY 3</a>
+            <a href="">CATEGORY 4</a>
+            <a href="">ABOUT US</a>
+            <a href="">CONTACT</a>
+
+        </nav>
+    </header>
+    <!-- SIDEBAR -->
+     <nav class="login__menu">
+        <h2>Enter website</h2>
+        <form action="" method="POST">
+            <label for="user">User:</label><br>
+            <input type="text" name="user"><br>
+
+            <label for="password">Password:</label><br>
+            <input type="password" name="password"><br>
+        </form>
+     </nav>
+
+    <!-- MAIN DIV -->
+
+
+    <!-- FOOTER -->
+</body>
+
+</html>
